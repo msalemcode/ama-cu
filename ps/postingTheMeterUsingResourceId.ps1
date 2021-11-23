02f7c8754f27159ab7eb627e1383044a3c62098a
dstarr <david@elegantcode.com>
2021-11-23 00:18:37 +0000

    formatting

--- ps/postingTheMeterUsingResourceId.ps1
+++ ps/postingTheMeterUsingResourceId.ps1
@@ -18,22 +18,21 @@ function Invoke-Meter() {
 
     # Get system identity access token
     # You will use this token for calling the management API
-    $managementTokenUrl = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F"
-    $Token = Invoke-RestMethod -Headers @{"Metadata" = "true" } -Uri $managementTokenUrl 
+    $ManagementTokenUrl = "http://169.254.169.254/Metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F"
+    $Token = Invoke-RestMethod -Headers @{"Metadata" = "true" } -Uri $ManagementTokenUrl 
 
+    # Get subscription and resource group
+    $MetadataUrl = "http://169.254.169.254/Metadata/instance?api-version=2019-06-01"
+    $Metadata = Invoke-RestMethod -Headers @{'Metadata' = 'true' } -Uri $MetadataUrl
+
+    # Make sure the system identity has at least reader permission on the resource group through the deployment template
     $Headers = @{
         "Authorization" = "$($Token.token_type) " + " " + "$($Token.access_token)"
         "Metadata"      = "true"
     }
-
-    # Get subscription and resource group
-    $metadataUrl = "http://169.254.169.254/metadata/instance?api-version=2019-06-01"
-    $metadata = Invoke-RestMethod -Headers @{'Metadata' = 'true' } -Uri $metadataUrl
-
-    # Make sure the system identity has at least reader permission on the resource group through the deployment template
-    $managementUrl = "https://management.azure.com/subscriptions/" + $metadata.compute.subscriptionId + "/resourceGroups/" + $metadata.compute.resourceGroupName + "?api-version=2019-10-01"
-    $resourceGroupInfo = Invoke-RestMethod -Headers $Headers -Uri $managementUrl 
-    $ManagedAppId = $resourceGroupInfo.managedBy
+    $ManagementUrl = "https://management.azure.com/subscriptions/" + $Metadata.compute.subscriptionId + "/resourceGroups/" + $Metadata.compute.resourceGroupName + "?api-version=2019-10-01"
+    $ResourceGroupInfo = Invoke-RestMethod -Headers $Headers -Uri $ManagementUrl 
+    $ManagedAppId = $ResourceGroupInfo.managedBy
 
     # Get ResourceUsageId from the managed app
     # A nested deployment within the template is used to give read access to the managed identity.
@@ -42,7 +41,7 @@ function Invoke-Meter() {
     $ResourceUsageId = $ManagedApp.properties.billingDetails.ResourceUsageId
 
     # Get the token for calling the metering API
-    $MeteringApiTokenUrl = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=20e940b3-4c77-4b0b-9a53-9e16a1b010a7"
+    $MeteringApiTokenUrl = "http://169.254.169.254/Metadata/identity/oauth2/token?api-version=2018-02-01&resource=20e940b3-4c77-4b0b-9a53-9e16a1b010a7"
     $Token = Invoke-RestMethod -Headers @{"Metadata" = "true" } -Uri $MeteringApiTokenUrl 
 
     # Set to use TLS 1.2
@@ -61,7 +60,6 @@ function Invoke-Meter() {
         "effectiveStartTime" = $LastHourMinusFiveMinutes
         "planId"             = $PlanId
     } | ConvertTo-Json
-
 
     # Post the meter
     $Response = Invoke-RestMethod 'https://marketplaceapi.microsoft.com/api/usageEvent?api-version=2018-08-31' -Method 'POST' -ContentType "application/json" -Headers $Headers -Body $Body -Verbose

8beae7b0a168ee6b87385e74a9ce66aeb33cc844
David Starr <david@elegantcode.com>
2022-01-19 16:18:43 -0800

    adding manual deployment script

--- function/deploy/arm/deploy.ps1
+++ function/deploy/arm/deploy.ps1
@@ -3,9 +3,11 @@ $ResourceGroupName = "amafunc01"
 $TemplateFile = ".\maintemplate.json" 
 $TemplateParameterFile = ".\maintemplate.parameters.json"
 
+New-AzResourceGroup $ResourceGroupName -Location "east us" 
 
 New-AzResourceGroupDeployment `
     -Name $TemplateDeploymentname `
     -ResourceGroupName $ResourceGroupName `
     -TemplateFile $TemplateFile `
     -TemplateParameterFile $TemplateParameterFile
+    

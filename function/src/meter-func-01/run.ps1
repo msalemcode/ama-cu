5b36062db70424cbc2f0da938e068e868f4af840
David Starr <david@elegantcode.com>
2022-01-25 16:26:00 -0800

    safety commit

--- function/src/meter-func-01/run.ps1
+++ function/src/meter-func-01/run.ps1
@@ -1,25 +1,43 @@
 using namespace System.Net
 
-# Input bindings are passed in via param block.
+# $ErrorActionPreference = "Stop"
+
 param($Request, $TriggerMetadata)
+
+Write-Host "FOO"
 
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
-# Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
+$DimensionId = $Request.Body.Dimension
+$PlanId = $Request.Body.PlanId
+$Quantity = $Request.Body.Quantity
+$EffectiveStartTime = $Request.Body.EffectiveStartTime
+
+$IdentityEndpoint = $Env:IDENTITY_ENDPOINT
+$IdentityHeader = $Env:IDENTITY_HEADER
+$ResouceGroupName = $Env:WEBSITE_RESOURCE_GROUP
+
+$resourceURI = "https://management.azure.com/"
+$tokenAuthURI = $IdentityEndpoint + "?resource=$resourceURI&api-version=2019-08-01"
+$tokenResponse = Invoke-RestMethod -Method Get -Headers @{"X-IDENTITY-HEADER" = "$IdentityHeader" } -Uri $tokenAuthURI
+
+
+# make the call to the metering API
+$headers = @{
+    "Authorization" = $tokenResponse.token_type + ' ' + $tokenResponse.token
 }
 
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-
-if ($name) {
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
+$body = @{
+    "quantity" = $Quantity
+    "dimension" = $DimensionId
+    "effectiveStartTime" = $EffectiveStartTime
+    "planId" = $PlanId
 }
 
-# Associate values to output bindings by calling 'Push-OutputBinding'.
+# $meterCallResponse = Invoke-RestMethod 'https://marketplaceapi.microsoft.com/api/usageEvent?api-version=2018-08-31' -Method 'POST' -ContentType "application/json" -Headers $headers -Body $body -Verbose
+
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
-    Body = $body
+    Body = ($body | ConvertTo-Json)
 })
